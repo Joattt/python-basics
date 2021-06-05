5cbdf8304cac7e633a70d07ca28294c3bc923f65
Joattt <84551066+Joattt@users.noreply.github.com>
2021-06-05 16:43:15 +1000

    Update utils.py

--- Batskalyov_Vadim_dz_4/utils.py
+++ Batskalyov_Vadim_dz_4/utils.py
@@ -7,8 +7,8 @@ def currency_rates(currency):
     response = get('http://www.cbr.ru/scripts/XML_daily.asp').text
     currency_idx = response.find((currency.upper()))
     if currency_idx != -1:
-        rate_idx = response.find('</Value>', currency_idx)
-        rate = Decimal(response[rate_idx - 7:rate_idx].replace(',', '.'))
+        rate_str = response[response.find('<Value>', currency_idx) + 7:response.find('</Value>', currency_idx)]
+        rate = Decimal(rate_str.replace(',', '.'))
         date_idx = response.find('Date="')
         day = int(response[date_idx + 6:date_idx + 8])
         month = int(response[date_idx + 9:date_idx + 11])
