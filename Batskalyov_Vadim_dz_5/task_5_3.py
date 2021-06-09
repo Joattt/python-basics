fd7a8741c0d623cfdbb261f32e83924d31dc301f
Joattt <84551066+Joattt@users.noreply.github.com>
2021-06-09 16:14:37 +1000

    Update task_5_3.py

--- Batskalyov_Vadim_dz_5/task_5_3.py
+++ Batskalyov_Vadim_dz_5/task_5_3.py
@@ -16,7 +16,7 @@
 tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
 klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
 
-my_gen = zip(tutors, klasses)
+my_gen = ((tutor, klass) for tutor, klass in zip(tutors, klasses))
 print(next(my_gen))
 print(next(my_gen))
 print(next(my_gen))
@@ -38,7 +38,7 @@ from itertools import zip_longest
 tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
 klasses = ['9А', '7В', '9Б', '9В', '8Б']
 
-my_gen = zip_longest(tutors, klasses)
+my_gen = ((tutor, klass) for tutor, klass in zip_longest(tutors, klasses))
 print(next(my_gen))
 print(next(my_gen))
 print(next(my_gen))
